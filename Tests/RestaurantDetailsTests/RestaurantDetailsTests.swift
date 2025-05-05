7eb09d8e8f819efee2b07d7c750ddbb8e17db424
Karthick <mail4karthickr@gmail.com>
2025-05-05 18:39:08 +0530

    Dummy tests

---
+++ Tests/RestaurantDetailsTests/RestaurantDetailsTests.swift
@@ -0,0 +1,38 @@
+//
+//  RestaurantDetailsTests.swift
+//  RestaurantDetails
+//
+//  Created by Karthick Ramasamy on 05/05/25.
+//
+
+import XCTest
+@testable import RestaurantDetails
+
+final class RestaurantDetailsTests: XCTestCase {
+    
+    func testExample1() {
+        XCTAssertTrue(true, "Dummy test 1 passed")
+    }
+    
+    func testExample2() {
+        XCTAssertEqual(2 + 2, 4, "Dummy test 2 passed")
+    }
+    
+    func testExample3() {
+        XCTAssertNotNil("Hello, World!", "Dummy test 3 passed")
+    }
+    
+    func testExample4() {
+        XCTAssertFalse(false, "Dummy test 4 passed")
+    }
+    
+    func testExample5() {
+        XCTAssertGreaterThan(10, 5, "Dummy test 5 passed")
+    }
+}//
+//  \RestaurantDetailsTests.swift
+//  RestaurantDetails
+//
+//  Created by Karthick Ramasamy on 05/05/25.
+//
+
